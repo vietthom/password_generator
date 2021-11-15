05079ff9e8d2cca3b31538bb7fa9f52e5a38d93d
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-11-14 17:02:45 -0700

    removed pseudo-code/comments

--- Develop/script.js
+++ Develop/script.js
@@ -75,7 +75,6 @@ function generatePassword(){
     return result;
     
   }
-
 };
 // Write password to the #password input
 function writePassword() {
@@ -85,40 +84,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-
-
-
-
-
-//GIVEN I need a new, secure password
-
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-
-//We need an eventlistener to generate a series of propt when we click a button we get a prompt for password criterias
-
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-
-
-
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
-
-
-
-
