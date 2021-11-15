2c0c6d8e0a7602e3cd23c3b88e0661330789f38f
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-11-14 17:16:01 -0700

    cleaned up js syntax

--- Develop/script.js
+++ Develop/script.js
@@ -9,63 +9,34 @@ const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '
 const lowerCase= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 const nums= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specChar = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "-",
-  "_",
-  "=",
-  "+",
-  "~",
-  "`",
-  "[",
-  "{",
-  "}",
-  "]",
-  "\\",
-  "|",
-  ";",
-  ":",
-  "'",
-  ",",
-  "<",
-  ".",
-  ">",
-  "/",
-  "?",
+  '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '~', '`', '[', '{', '}', ']', '\\', '|', ';', ':', '"', ',', '<', '.', '>', '/', '?',
 ];
 
 function generatePassword(){
-  passwordLength = prompt('Please chose a password length of 8 to 128 characters');
+  passwordLength = prompt('Please chose a password length of 8 to 128 characters.');
   if (passwordLength < 8 || passwordLength > 128){
-    passwordLength = prompt("Password length must be 8 to 128 characters");
+    passwordLength = prompt('Password length MUST be 8 to 128 characters.');
   }else{
-    let addNumbers = confirm("Would you like to add numbers to your password?");{
+    let addNumbers = confirm('Would you like to add numbers to your password?');
+    {
       if (addNumbers){
         userInputs= userInputs.concat(nums);
       }
     }
-    let addUpperCase = confirm("Would you like to add uppercase letters to your password?");
+    let addUpperCase = confirm('Would you like to add uppercase letters to your password?');
     if(addUpperCase){
       userInputs = userInputs.concat(upperCase);
     }
-    let addLowerCase = confirm("Would you like to add lowercase letters to your password");
+    let addLowerCase = confirm('Would you like to add lowercase letters to your password');
     if(addLowerCase){
       userInputs = userInputs.concat(lowerCase);
     }
-    let addSpecialCharacters = confirm("Would you like to add special characters to your password?");
+    let addSpecialCharacters = confirm('Would you like to add special characters to your password?');
     if(addSpecialCharacters){
       userInputs = userInputs.concat(specChar);
     }
     if (!addNumbers && !addUpperCase && !addLowerCase && !addSpecialCharacters){
-      alert('No password created');
+      alert('No password created!');
     };
     let result = '';
     for(let i=0; i < passwordLength; i++){
